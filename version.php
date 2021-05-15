65583f642f2b0e5e67ca4a56bea147ea9deacdc6
Veloxnet-LMS <lms@veloxnet.hu>
2021-05-15 14:01:27 +0200

    Update version.php

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2021050600';
-$plugin->requires = '2016070700';
+$plugin->version = 2021050600;
+$plugin->requires = 2016070700;
 $plugin->component = 'theme_vxg_boost';
 $plugin->dependencies = [
-    'theme_boost' => '2016102100'
+    'theme_boost' => 2016102100
 ];
